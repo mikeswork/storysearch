5f046a26e33e4b124aa25b6c018d9bbbd2b935a1
Mike Richardson <mike.richardson@exponentialent.com>
2020-12-24 01:24:22 -0800

    - Changed clipping logic to use percentage at 10% increments - Tweaked visual state during interaction - Added touch behavior (e.g. mobile) - Added Submit Score button but MongoDB logic is still needed

--- src/components/RateTool.tsx
+++ src/components/RateTool.tsx
@@ -1,58 +1,97 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import pensoutlined from "../assets/pens-outlined.png";
 import pensfilled from "../assets/pens-filled.png";
 
-const rateIncrSize = 30;
-
 const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    padding: 1em;
-    background-image: url(${pensoutlined});
-    background-repeat: no-repeat;
-    background-position: 50%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 1em;
+	background-color: white;
 
-    background-color: white;
-    ${(props) =>
-        props.$ratingLocked && `background-color: #bbb;`
-    }
+	.icons {
+		background-image: url(${pensoutlined});
+		background-repeat: no-repeat;
+		background-position: 50%;
+		opacity: 0.6;
+	}
+
+	${(props) =>
+		props.$isScored &&
+		`
+            .icons {opacity: 1.0}
+        `}
 `;
 
 const ClippedImg = styled.img`
-    display: block;
+	display: block;
 
-    ${(props) => 
-        props.$clipRight &&
-        `
-            clip-path: polygon(0 0, ${props.$clipRight}px 0, ${props.$clipRight}px 100%, 0 100%);
-        `
-    }
+	${(props) =>
+		props.$clipRight &&
+		`
+            clip-path: polygon(0 0, ${props.$clipRight} 0, ${props.$clipRight} 100%, 0 100%);
+        `}
 `;
 
 export default function RateTool(props) {
-    const [mouseX, setMouseX] = useState(0);
-    const [rateLocked, setRateLocked] = useState<boolean>();
+	const score = useRef<number>();
+	const [clipPrcnt, setClipPrcnt] = useState("0%");
+	const [isScored, setIsScored] = useState<boolean>();
 
-    const slideRating = (e) => {
-        // console.log(e)
-        if (!rateLocked) {
-            const newMouseX = e.clientX - e.target.offsetLeft;
-            const width = e.target.clientWidth;
+	const slideRating = (e) => {
+		// console.log(e)
+		if (!isScored) {
+			// Score is 1 - 10
+			score.current = Math.round(((e.clientX - e.target.offsetLeft) / e.target.clientWidth) * 10);
 
-            if (newMouseX + rateIncrSize >= width) setMouseX(width)
-            else if (newMouseX - rateIncrSize < 0) setMouseX(1)
-            else if (Math.abs(newMouseX - mouseX) >= rateIncrSize) setMouseX(newMouseX)
-        }
-    }
+			// Convert score to percentage
+			setClipPrcnt(`${score.current * 10}%`);
+		}
+	};
 
-    const toggleRating = () => setRateLocked(!rateLocked);
+	const toggleIsScored = (e) => {
+		setIsScored(!isScored);
+		console.log(e);
+	};
 
-    return (
-        <Wrapper $ratingLocked={rateLocked}>
-            <div onMouseMove={slideRating} onClick={toggleRating}>
-                <ClippedImg src={pensfilled} alt="Rate the story" $clipRight={mouseX} />
-            </div>
-        </Wrapper>
-    )
+	const endRating = () => {
+		if (!isScored) {
+			score.current = 0;
+			setClipPrcnt(`0%`);
+		}
+	};
+
+	const touchRating = (e) => {
+		e.preventDefault();
+
+		// Score is 1 - 10
+		score.current = Math.round(((e.changedTouches[0].clientX - e.target.offsetLeft) / e.target.clientWidth) * 10);
+
+		// Convert score to percentage
+		setClipPrcnt(`${score.current * 10}%`);
+		setIsScored(true);
+	};
+
+	const submitScore = () => {
+		console.error(`Need to implement submitting of score (current is ${score.current})`);
+	};
+
+	return (
+		<Wrapper $isScored={isScored}>
+			<div
+				className="icons"
+				onMouseMove={slideRating}
+				onClick={toggleIsScored}
+				onMouseLeave={endRating}
+				onTouchEnd={touchRating}
+			>
+				<ClippedImg src={pensfilled} alt="Rate the story" $clipRight={clipPrcnt} />
+			</div>
+
+			<button onClick={submitScore} disabled={!isScored}>
+				Submit Score
+			</button>
+		</Wrapper>
+	);
 }
